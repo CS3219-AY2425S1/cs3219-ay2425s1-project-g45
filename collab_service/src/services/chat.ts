7fe268d003e2da5b5cafb8e5a06f3d00da89768a
Zheng Ze <zz.phua@gmail.com>
2024-10-27 21:51:38 +0800

    Define new chat types

---
+++ collab_service/src/services/chat.ts
@@ -0,0 +1,49 @@
+import { ChatState } from "peerprep-shared-types";
+import { getRoomMessages, updateMessages } from "./roomService";
+
+export class ChatManager {
+  private roomChatStates = new Map<string, ChatState>();
+
+  initialiseChat(roomId: string) {
+    if (!this.roomChatStates.has(roomId)) {
+      console.log("Initializing chat for room", roomId);
+      getRoomMessages(roomId).then((messages) => {
+        this.roomChatStates.set(roomId, { messages: messages || [] });
+      });
+    }
+    console.log("roomChatStates.get(roomId)", this.roomChatStates.get(roomId));
+    return this.roomChatStates.get(roomId)!;
+  }
+
+  addMessage(roomId: string, message: string, username: string) {
+    const state = this.roomChatStates.get(roomId);
+    if (state) {
+      state.messages.push({
+        userId: username,
+        message: message,
+        timestamp: new Date(),
+      });
+      return state;
+    }
+    return null;
+  }
+
+  getChatState(roomId: string) {
+    return this.roomChatStates.get(roomId);
+  }
+
+  cleanupChat(roomId: string) {
+    if (this.roomChatStates.has(roomId)) {
+      updateMessages(roomId, this.roomChatStates.get(roomId).messages);
+    }
+    return this.roomChatStates.delete(roomId);
+  }
+
+  getChatHistory(roomId: string) {
+    const state = this.roomChatStates.get(roomId);
+    if (state) {
+      return state.messages;
+    }
+    return null;
+  }
+}
