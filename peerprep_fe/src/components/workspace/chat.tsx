480109f831a0f99c21f2c347a2b8a083b4f507d7
Zheng Ze <zz.phua@gmail.com>
2024-10-29 03:04:36 +0800

    Add background to chat message and add new message notification

--- peerprep_fe/src/components/workspace/chat.tsx
+++ peerprep_fe/src/components/workspace/chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ChatMessage } from "peerprep-shared-types";
 import { useAuth } from "@/contexts/auth-context";
 import Textfield from "../common/text-field";
@@ -15,20 +15,51 @@ const Chat: React.FC<ChatProps> = ({
 }) => {
   const { username } = useAuth();
   const [messageToSend, setMessageToSend] = useState("");
+  const [messageCount, setMessageCount] = useState(0);
+  const [notification, setNotification] = useState(false);
+
+  useEffect(() => {
+    if (
+      messages.length > messageCount &&
+      messages[messages.length - 1].username !== username
+    ) {
+      setNotification(true);
+    }
+    setMessageCount(messages.length);
+  }, [messages]);
+
+  function handleScroll(e: React.UIEvent<HTMLDivElement, UIEvent>) {
+    const bottom =
+      e.currentTarget.scrollHeight - e.currentTarget.scrollTop - 2 <=
+      e.currentTarget.clientHeight;
+    if (bottom) {
+      console.log("At the bottom");
+      setNotification(false);
+    }
+  }
+
   return (
     <div className="h-full bg-white dark:bg-slate-800 rounded-lg flex flex-col">
-      <div className="max-h-full w-full overflow-y-scroll flex-grow">
-        <div className="flex flex-col-reverse px-4">
+      <div
+        id="chat-container"
+        className="max-h-full w-full overflow-y-scroll flex-grow"
+        onScroll={(e) => handleScroll(e)}
+      >
+        {notification && (
+          <div className="sticky top-0 text-center text-gray-500 dark:text-gray-400 ">
+            {"New Messages"}
+          </div>
+        )}
+        <div className="flex flex-col-reverse justify-end h-full px-4">
           {messages &&
             messages.toReversed().map((message, index) => {
               return (
                 <div
                   key={index}
-                  className={`w-full mb-4 ${username === message.username ? "justify-items-end" : ""}`}
+                  className={`w-fit mb-4 bg-green-800 rounded-lg p-2
+                    ${username === message.username ? "self-end place-items-end" : ""}`}
                 >
-                  <div
-                    className={`text-sm font-medium text-gray-900 dark:text-slate-300 ${username === message.username ? "justify-self-end" : ""}`}
-                  >
+                  <div className="text-sm font-medium text-gray-900 dark:text-slate-300">
                     {message.username}
                   </div>
                   <div className="text-sm text-gray-500 dark:text-zinc-400 text-wrap">
@@ -38,6 +69,7 @@ const Chat: React.FC<ChatProps> = ({
               );
             })}
         </div>
+        {/* Signal to user if new messages were received */}
       </div>
       <div className="flex justify-between space-x-5 items-center px-4">
         <div className="flex-grow">
