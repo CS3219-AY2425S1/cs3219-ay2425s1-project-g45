e4427a45856cf82040f4cd88932eb9652720e8f1
Jun Jie <62635032+TJun-Jie@users.noreply.github.com>
2024-11-02 19:51:17 +0800

    fix FE types

--- peerprep_fe/tailwind.config.ts
+++ peerprep_fe/tailwind.config.ts
@@ -7,17 +7,6 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    fontWeight: {
-      hairline: 100,
-      thin: 200,
-      light: 300,
-      normal: 400,
-      medium: 500,
-      semibold: 600,
-      bold: 700,
-      extrabold: 800,
-      black: 900,
-    },
     extend: {
       colors: {
         background: "var(--background)",
